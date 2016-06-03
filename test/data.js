beb6397a5696dd396e7b4e4f59aa067980f37d48
Matthew Davies <daviesgeek@icloud.com>
2016-06-03 15:59:59 -0700

    Added tests

---
+++ test/data.js
@@ -0,0 +1,124 @@
+var data = {
+  users: [
+  {
+    "id": 1,
+    "first_name": "Jose",
+    "last_name": "Ellis",
+    "email": "jellis0@japanpost.jp",
+    "country": "Indonesia",
+    "ip_address": "202.88.170.138"
+  },
+  {
+    "id": 2,
+    "first_name": "Rebecca",
+    "last_name": "Arnold",
+    "email": "rarnold1@timesonline.co.uk",
+    "country": "Portugal",
+    "ip_address": "214.52.80.16"
+  },
+  {
+    "id": 3,
+    "first_name": "Annie",
+    "last_name": "Grant",
+    "email": "agrant2@rakuten.co.jp",
+    "country": "Norway",
+    "ip_address": "119.155.7.212"
+  },
+  {
+    "id": 4,
+    "first_name": "Nicholas",
+    "last_name": "Jordan",
+    "email": "njordan3@sourceforge.net",
+    "country": "Germany",
+    "ip_address": "152.150.196.26"
+  },
+  {
+    "id": 5,
+    "first_name": "Wayne",
+    "last_name": "Morales",
+    "email": "wmorales4@ted.com",
+    "country": "China",
+    "ip_address": "146.78.9.169"
+  },
+  {
+    "id": 6,
+    "first_name": "Julia",
+    "last_name": "Roberts",
+    "email": "jroberts5@newyorker.com",
+    "country": "China",
+    "ip_address": "92.13.152.17"
+  },
+  {
+    "id": 7,
+    "first_name": "Mildred",
+    "last_name": "Perez",
+    "email": "mperez6@w3.org",
+    "country": "Nigeria",
+    "ip_address": "251.61.36.174"
+  },
+  {
+    "id": 8,
+    "first_name": "Barbara",
+    "last_name": "Matthews",
+    "email": "bmatthews7@apache.org",
+    "country": "Morocco",
+    "ip_address": "249.52.199.231"
+  },
+  {
+    "id": 9,
+    "first_name": "Thomas",
+    "last_name": "Stephens",
+    "email": "tstephens8@springer.com",
+    "country": "Peru",
+    "ip_address": "125.231.3.154"
+  },
+  {
+    "id": 10,
+    "first_name": "Chris",
+    "last_name": "Alvarez",
+    "email": "calvarez9@psu.edu",
+    "country": "Paraguay",
+    "ip_address": "165.86.187.20"
+  },
+  {
+    "id": 11,
+    "first_name": "Jeremy",
+    "last_name": "Anderson",
+    "email": "jandersona@unc.edu",
+    "country": "Bulgaria",
+    "ip_address": "196.24.19.91"
+  },
+  {
+    "id": 12,
+    "first_name": "Sara",
+    "last_name": "Reed",
+    "email": "sreedb@economist.com",
+    "country": "Finland",
+    "ip_address": "180.92.97.216"
+  },
+  {
+    "id": 13,
+    "first_name": "Roger",
+    "last_name": "Fowler",
+    "email": "rfowlerc@wix.com",
+    "country": "Russia",
+    "ip_address": "178.200.193.110"
+  },
+  {
+    "id": 14,
+    "first_name": "Martha",
+    "last_name": "Gibson",
+    "email": "mgibsond@topsy.com",
+    "country": "China",
+    "ip_address": "215.240.201.105"
+  },
+  {
+    "id": 15,
+    "first_name": "Samuel",
+    "last_name": "Kennedy",
+    "email": "skennedye@soundcloud.com",
+    "country": "Libya",
+    "ip_address": "248.232.220.200"
+  }
+]
+}
---
+++ test/test.js
@@ -0,0 +1,199 @@
+'use strict';
+let expect = chai.expect
+
+describe('RestJS', function () {
+
+  var User, server;
+
+  beforeEach(function () {
+    Rest.Config.set({
+      baseUrl: 'http://server.com',
+      withCredentials: true,
+    })
+
+    User = Rest.factory('users')
+
+    server = sinon.fakeServer.create()
+    server.autoRespond = true
+  })
+
+  afterEach(function () {
+    server.restore()
+  })
+
+  it('should create a factory', function () {
+    expect(User).to.have.property('getList')
+    expect(User).to.have.property('get')
+  })
+
+  it('should create an element using \'create\'', function () {
+    let user = User.create({id: 1, first_name: "Matthew"})
+    shouldBeRestified(user, 'users', false)
+  })
+
+  it('should make a get request', function (done) {
+    server.respondWith(function (request) {
+      expect(request.method).to.equal('GET')
+      expect(request.url).to.equal('http://server.com/users')
+      expect(request.responseType).to.equal('json')
+      request.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(data.users))
+    })
+    User.getList().then(function (users) {
+      expect(users).to.exist
+      users.forEach(function (user) {
+        shouldBeRestified(user, 'users')
+      })
+      done()
+    }, done)
+  })
+
+  it('should make a get request for a single element', function (done) {
+    server.respondWith(function (request) {
+      expect(request.method).to.equal('GET')
+      expect(request.url).to.equal('http://server.com/users/1')
+      expect(request.responseType).to.equal('json')
+      request.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(data.users[0]))
+    })
+
+    User.get(1).then(function(user) {
+      expect(user).to.exist
+      expect(user.id).to.equal(1)
+      shouldBeRestified(user, 'users')
+      done()
+    })
+  })
+
+  it('should make a post request', function (done) {
+    server.respondWith(function (request) {
+      expect(request.method).to.equal('POST')
+      expect(request.url).to.equal('http://server.com/users')
+      expect(request.responseType).to.equal('json')
+      expect(request.requestBody).to.equal(JSON.stringify({"first_name": "Jose", "last_name": "Ellis", "email": "jellis0@japanpost.jp", "country": "Indonesia", "ip_address": "202.88.170.138"}))
+
+      let returnUser = data.users[0]
+      returnUser.id = 42
+
+      request.respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(returnUser))
+    })
+
+    User.create({
+      "first_name": "Jose",
+      "last_name": "Ellis",
+      "email": "jellis0@japanpost.jp",
+      "country": "Indonesia",
+      "ip_address": "202.88.170.138"
+    }).post().then(function (user) {
+      expect(user.id).to.equal(42)
+      shouldBeRestified(user, 'users')
+      done()
+    })
+
+  })
+
+  describe('should make a patch request', function () {
+
+    let user = Rest.factory('users').create({id: 1, first_name: "John", last_name: "Smith"})
+
+    shouldBeRestified(user, 'users', false)
+
+    it('with an object', function (done) {
+      server.respondWith(function (request) {
+        expect(request.method).to.equal('PATCH')
+        expect(request.url).to.equal('http://server.com/users/1')
+        expect(request.responseType).to.equal('json')
+        expect(request.requestBody).to.equal(JSON.stringify({first_name: "John"}))
+
+        request.respond(200, { 'Content-Type': 'application/json' }, request.requestBody)
+      })
+
+      user.patch({first_name: "John"}).then(function (data) {
+        expect(data).to.deep.equal(user)
+        done()
+      })
+
+      shouldBeRestified(user, 'users', false)
+    })
+
+    it('with an object and params', function (done) {
+      server.respondWith(function (request) {
+        expect(request.method).to.equal('PATCH')
+        expect(request.url).to.equal('http://server.com/users/1?with=all')
+        expect(request.responseType).to.equal('json')
+        expect(request.requestBody).to.equal(JSON.stringify({first_name: "John"}))
+
+        request.respond(200, { 'Content-Type': 'application/json' }, request.requestBody)
+      })
+
+      user.patch({first_name: "John"}, {with: 'all'}).then(function (data) {
+        expect(data).to.deep.equal(user)
+        done()
+      })
+
+      shouldBeRestified(user, 'users', false)
+    })
+
+    it('with string properties', function (done) {
+      server.respondWith(function (request) {
+        expect(request.method).to.equal('PATCH')
+        expect(request.url).to.equal('http://server.com/users/1')
+        expect(request.responseType).to.equal('json')
+        expect(request.requestBody).to.equal(JSON.stringify({id: 1, first_name: "John", last_name: "Smith"}))
+
+        request.respond(200, { 'Content-Type': 'application/json' }, request.requestBody)
+      })
+
+      user.patch('id', 'first_name', 'last_name').then(function (data) {
+        expect(data).to.deep.equal(user)
+        done()
+      })
+
+      shouldBeRestified(user, 'users', false)
+    })
+
+    it('with string properties and params', function (done) {
+      server.respondWith(function (request) {
+        expect(request.method).to.equal('PATCH')
+        expect(request.url).to.equal('http://server.com/users/1?with=all')
+        expect(request.responseType).to.equal('json')
+        expect(request.requestBody).to.equal(JSON.stringify({id: 1, first_name: "John", last_name: "Smith"}))
+
+        request.respond(200, { 'Content-Type': 'application/json' }, request.requestBody)
+      })
+
+      user.patch('id', 'first_name', 'last_name', {with: 'all'}).then(function (data) {
+        expect(data).to.deep.equal(user)
+        done()
+      })
+
+      shouldBeRestified(user, 'users', false)
+    })
+
+  })
+
+  it('should make a put request', function (done) {
+    server.respondWith(function (request) {
+      expect(request.method).to.equal('PUT')
+      expect(request.url).to.equal('http://server.com/users/1')
+      expect(request.responseType).to.equal('json')
+      expect(request.requestBody).to.equal(JSON.stringify({id: 1, first_name: "John", last_name: "Smith"}))
+
+      request.respond(200, { 'Content-Type': 'application/json' }, request.requestBody)
+    })
+
+    let user = User.create({id: 1, first_name: "John", last_name: "Smith"})
+    user.put().then(function (data) {
+      expect(data).to.deep.equal(user)
+      done()
+    })
+  })
+
+  function shouldBeRestified(element, route, fromServer=true) {
+    expect(element).to.exist
+    expect(element).to.have.property('get')
+    expect(element).to.have.property('post')
+    expect(element).to.have.property('patch')
+    expect(element).to.have.property('put')
+    expect(element).to.have.ownPropertyDescriptor('route', {enumerable: false, configurable: false, writable: false, value: route})
+    expect(element).to.have.ownPropertyDescriptor('fromServer', {enumerable: false, configurable: false, writable: false, value: fromServer})
+  }
+})
