38a42d2b300591bce71c4c6ec19e2756e118614e
Matthew Davies <daviesgeek@icloud.com>
2016-09-23 16:20:49 -0700

    Updated release script to include docs for the website

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,12 @@
 npm run local-test
 npm run build
 node_modules/.bin/json -I -f package.json -e 'this.version="'$@'"'
+npm run docs
+mv "docs/restjs/$@" "docs/$@"
+rm -rf docs/restjs
+git checkout gh-pages
+cp -R "docs/$@/*" docs
+git add "docs"
+git commit -m "Added docs for $@"
+git checkout master
 echo "All done. Don't forget to update the changelog, commit, tag and push!"
