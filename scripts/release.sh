b81475e174421450b3add0fee08768e0f3a76423
Matthew Davies <daviesgeek@icloud.com>
2017-02-10 14:19:43 -0800

    Updated release script to fix docs latest

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,8 @@ node_modules/.bin/json -I -f package.json -e 'this.version="'$@'"'
 npm run docs
 mv "docs/restjs/$@" "docs/$@"
 rm -rf docs/restjs
-git checkout gh-pages
-cp -R "docs/$@/*" docs
+cat docs/latest/index.template.md > docs/latest/index.md
+echo "<script>window.location = '/$@'</script>" >> docs/latest/index.md
 git add "docs"
 git commit -m "Added docs for $@"
-git checkout master
 echo "All done. Don't forget to update the changelog, commit, tag and push!"
